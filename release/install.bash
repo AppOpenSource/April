63715331ee297d8fe108a0646981a1a366548534
huangweiqi <askviky2010@gmail.com>
2018-06-03 18:26:11 +0800

    添加安装脚本

---
+++ release/install.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo 'adb uninstall com.abt.clock_memo'
+adb uninstall com.abt.clock_memo
+echo 'apk uninstalled'
+
+echo 'adb install'
+adb install /Users/huangweiqi/StudioProjects/April/release/ClockMemo_V2.0.1_20180603_B.apk
+echo 'apk installed'
