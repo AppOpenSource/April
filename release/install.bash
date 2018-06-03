bbd44a723a5925f4c847d7ec04b02fb7f6c2012a
huangweiqi <askviky2010@gmail.com>
2018-06-04 01:03:56 +0800

    发布V2.0.2

--- release/install.bash
+++ release/install.bash
@@ -4,5 +4,5 @@ adb uninstall com.abt.clock_memo
 echo 'apk uninstalled'
 
 echo 'adb install'
-adb install /Users/huangweiqi/StudioProjects/April/release/ClockMemo_V2.0.1_20180603_B.apk
+adb install /Users/huangweiqi/StudioProjects/April/release/ClockMemo_V2.0.2_20180603_B.apk
 echo 'apk installed'
--- release/release_note.md
+++ release/release_note.md
@@ -2,6 +2,9 @@
 ##  ClockMemo (打卡备忘) RELEASE_NOTE   
 
 ### NextVersion
+1、
+
+### ClockMemo_V2.0.2_20180603_B.apk   
 1、添加版本信息展示    
 2、添加自动检测版本更新     
 3、解决加载更多的Bug  
