b350d6f0d080b4dbac8a5079dc4459345122a928
huangweiqi <askviky2010@gmail.com>
2018-06-10 14:20:30 +0800

    添加gank的bean

---
+++ price/src/main/java/com/abt/price/bean/gank/Video.java
@@ -0,0 +1,31 @@
+package com.abt.price.bean.gank;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @描述： @Video
+ * @作者： @黄卫旗
+ * @创建时间： @10/06/2018
+ */
+public class Video implements Serializable {
+
+    private boolean error;
+    private List<Gank> results;
+
+    public boolean isError() {
+        return error;
+    }
+
+    public List<Gank> getResults() {
+        return results;
+    }
+
+    @Override
+    public String toString() {
+        return "Video{" +
+                "error=" + error +
+                ", results=" + results +
+                '}';
+    }
+}
