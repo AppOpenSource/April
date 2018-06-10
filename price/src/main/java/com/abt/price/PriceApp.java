b44dac7a073855ccef17fcffe7879cc74d066824
huangweiqi <askviky2010@gmail.com>
2018-06-10 13:54:39 +0800

    添加priceApp

---
+++ price/src/main/java/com/abt/price/PriceApp.java
@@ -0,0 +1,16 @@
+package com.abt.price;
+
+import com.abt.common.app.BasicApplication;
+
+/**
+ * @描述： @PriceApp
+ * @作者： @黄卫旗
+ * @创建时间： @10/06/2018
+ */
+public class PriceApp extends BasicApplication {
+
+    @Override
+    public void initComplete() {
+
+    }
+}
